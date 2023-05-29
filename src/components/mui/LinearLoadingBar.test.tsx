157849737cddf65e9f97634557c502c2114de3b7
Sinan Bolel <1915925+sbolel@users.noreply.github.com>
2023-05-29 11:32:17 -0400

    test: remove unused imports in tests

--- src/components/mui/LinearLoadingBar.test.tsx
+++ src/components/mui/LinearLoadingBar.test.tsx
@@ -1,12 +1,9 @@
-import * as React from 'react'
 import { render } from '@testing-library/react'
 import LinearLoadingBar from '@/components/mui/LinearLoadingBar'
 
 describe('LinearLoadingBar', () => {
   test('renders correctly', () => {
     const { getByRole } = render(<LinearLoadingBar />)
-
-    // Expect the LinearProgress component to be in the document
     expect(getByRole('progressbar')).toBeInTheDocument()
   })
 })
