1f560794266fee2cd71c1269adf68665f7dd04ce
Sinan Bolel <1915925+sbolel@users.noreply.github.com>
2023-05-29 11:51:37 -0400

    chore: fix naming and esdocs

--- src/actions/loginUserFederated.tsx
+++ src/actions/loginUserFederated.tsx
@@ -1,18 +1,16 @@
 /**
- * @module @sbom-harbor-ui/dashboard/actions/loginUser
+ * @module @sbom-harbor-ui/dashboard/actions/loginUserFederated
  */
 import React from 'react'
 import { Auth } from 'aws-amplify'
 import { AuthActions } from '@/actions/actionTypes'
-import { LoginParams } from '@/hooks/types'
 
-export default async function loginUser(
+export default async function loginUserFederated(
   dispatch: React.Dispatch<{
     type: AuthActions
     payload?: unknown
     error?: Error
-  }>,
-  payload: LoginParams
+  }>
 ) {
   try {
     dispatch({ type: AuthActions.LOGIN_REQUEST })
@@ -21,32 +19,31 @@ export default async function loginUser(
       .getAccessToken()
       .getJwtToken()
 
-    if (jwtToken) {
-      const {
-        accessKeyId,
-        sessionToken,
-        secretAccessKey,
-        identityId,
-        authenticated,
-        expiration,
-      } = user
-
-      const data = {
-        accessKeyId,
-        sessionToken,
-        secretAccessKey,
-        identityId,
-        authenticated,
-        expiration,
-      }
-
-      dispatch({ type: AuthActions.LOGIN_SUCCESS, payload: data })
-      return data
+    if (!jwtToken) {
+      throw new Error('No JWT token found')
     }
 
-    return
+    const {
+      accessKeyId,
+      sessionToken,
+      secretAccessKey,
+      identityId,
+      authenticated,
+      expiration,
+    } = user
+
+    const data = {
+      accessKeyId,
+      sessionToken,
+      secretAccessKey,
+      identityId,
+      authenticated,
+      expiration,
+    }
+
+    dispatch({ type: AuthActions.LOGIN_SUCCESS, payload: data })
+    return data
   } catch (error) {
-    console.warn(`Login failed for ${payload.email}`, error)
     dispatch({ type: AuthActions.LOGIN_FAILURE, error: error as Error })
   }
 }
