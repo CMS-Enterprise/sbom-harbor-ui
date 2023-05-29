0c7cbd9af08ca9c982d0ee309af0093832f44cba
Sinan Bolel <1915925+sbolel@users.noreply.github.com>
2023-05-29 11:11:03 -0400

    test(actions): fix type error

--- src/actions/logoutUser.tsx
+++ src/actions/logoutUser.tsx
@@ -3,11 +3,9 @@
  */
 import * as React from 'react'
 import { Auth } from 'aws-amplify'
-import { AuthActions, AuthActionType } from '@/actions/actionTypes'
+import { AuthActions } from '@/actions/actionTypes'
 
-export default async function logoutUser(
-  dispatch: React.Dispatch<AuthActionType>
-) {
+export default async function logoutUser(dispatch: React.Dispatch<any>) {
   await Auth.signOut()
   dispatch({ type: AuthActions.LOGOUT })
 }
