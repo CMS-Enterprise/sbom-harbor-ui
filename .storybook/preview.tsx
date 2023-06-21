832b6653289d6be631f8599a4b995db53789876e
Sinan Bolel <1915925+sbolel@users.noreply.github.com>
2023-06-21 14:26:12 -0400

    fix(storybook): use theme in storybook

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { withThemeFromJSXProvider } from '@storybook/addon-styling'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import theme from '../src/theme/theme'
 
 export const decorators = [
-  (Story) => (
-    <Story />
-  ),
-]
+  withThemeFromJSXProvider({
+  themes: {
+    light: theme,
+    dark: theme,
+  },
+  defaultTheme: 'light',
+  Provider: ThemeProvider,
+  GlobalStyles: CssBaseline,
+})];
