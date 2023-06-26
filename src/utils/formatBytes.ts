03b46b94589ba3662a4141bd3a0b6f6916706659
Sinan Bolel <1915925+sbolel@users.noreply.github.com>
2023-06-25 22:22:51 -0400

    test(utils): cover formatBytes

--- src/utils/formatBytes.ts
+++ src/utils/formatBytes.ts
@@ -1,14 +1,22 @@
+/**
+ * A utility function to format bytes into a human readable format.
+ * @module sbom-harbor-ui/utils/formatBytes
+ */
 const k = 1024
 const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
 
-function formatBytes(bytes: number, decimals = 0): string {
+/**
+ * A utility function to format bytes into a human readable format.
+ * @param {number} bytes the bytes value to format
+ * @param {[number=0]} decimals the number of decimal places to include
+ * @returns {string} bytes formatted into a human readable format
+ */
+const formatBytes = (bytes: number, decimals = 0): string => {
   if (!+bytes) {
     return '0 Bytes'
   }
-
   const dm = decimals < 0 ? 0 : decimals
   const i = Math.floor(Math.log(bytes) / Math.log(k))
-
   return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`
 }
 
