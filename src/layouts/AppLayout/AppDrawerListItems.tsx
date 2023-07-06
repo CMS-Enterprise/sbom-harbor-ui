d7f0ffb4d93c8f8c4128e7cc5454ebbe88760268
Sinan Bolel <1915925+sbolel@users.noreply.github.com>
2023-07-05 23:39:19 -0400

    style: improve app layout

--- src/layouts/AppLayout/AppDrawerListItems.tsx
+++ src/layouts/AppLayout/AppDrawerListItems.tsx
@@ -1,15 +1,14 @@
 /**
  * @module sbom-harbor-ui/layouts/AppLayout/AppDrawerListItems
  */
-import * as React from 'react'
 import DashboardIcon from '@mui/icons-material/Dashboard'
 import AppDrawerButton from '@/components/AppDrawerButton'
 
 const AppDrawerButtonList = () => (
-  <React.Fragment>
+  <>
     {/* Dashboard */}
     <AppDrawerButton label="Dashboard" to="" icon={<DashboardIcon />} />
-  </React.Fragment>
+  </>
 )
 
 export default AppDrawerButtonList
