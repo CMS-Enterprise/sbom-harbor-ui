4ef604ae94a633b9fd01d9e0893e5f3c8f323777
Sinan Bolel <1915925+sbolel@users.noreply.github.com>
2023-07-26 02:41:48 -0400

    build(vite): add missing vars to vite-env

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,10 +3,12 @@
 interface ImportMetaEnv {
   readonly VITE_APP_TITLE: string
   readonly VITE_CF_DOMAIN: string
+  readonly VITE_AWS_REGION: string
+  readonly VITE_COGNITO_DOMAIN: string
   readonly VITE_USER_POOL_ID: string
   readonly VITE_USER_POOL_CLIENT_ID: string
-  readonly VITE_AWS_REGION: string
-  // more env variables...
+  readonly VITE_COGNITO_REDIRECT_SIGN_IN: string
+  readonly VITE_COGNITO_REDIRECT_SIGN_OUT: string
 }
 
 interface ImportMeta {
